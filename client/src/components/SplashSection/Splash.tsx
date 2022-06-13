16c40c8ba28d832b1b231e8ff7042634d61881d0
Konrad Gruzlo <gruzlokonrad@gmail.com>
2022-06-13 15:25:43 +0200

    fix bugs of splash section

--- client/src/components/SplashSection/Splash.tsx
+++ client/src/components/SplashSection/Splash.tsx
@@ -1,16 +1,26 @@
-import './Splash.scss';
+import style from './Splash.module.scss';
 import Container from '../Container/Container';
 import Button from '../Button/Button';
 
 const Splash = () => {
   return (
-    <div className="splashSection">
+    <div className={style.splashSection}>
       <Container>
-        <div className="content">
+        <div className={style.splashSection__container}>
+
+
           <h1>Lorem Ipsum</h1>
-          <hr />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
           <Button>Zaloguj się</Button>
+
+
+
+          {/* <div className={style.splashSection__header}>
+          </div>
+          <div className={style.splashSection__content}>
+          </div>
+          <div className={style.splashSection__button}>
+          </div> */}
         </div>
       </Container>
     </div>
